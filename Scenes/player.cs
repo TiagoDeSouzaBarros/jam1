b388a7f236979e75d7656f666bda39732e35b705
Tiago Barros <ogaitsorrab@gmail.com>
2023-04-12 07:41:24 -0300

    rotation things

--- Scenes/player.cs
+++ Scenes/player.cs
@@ -6,9 +6,17 @@ public partial class player : CharacterBody2D
 	public const float Speed = 300.0f;
 	public const float JumpVelocity = -400.0f;
 
+	[Export]
+	public PackedScene GunWeapon;
+
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
 	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
-
+	public Node2D corpo;
+	public Sprite2D weapon;
+	public override void _Ready(){
+		corpo = GetNode<Node2D>("Char");
+		weapon = GetNode<Sprite2D>("WeaponGun");
+	}
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
@@ -34,6 +42,10 @@ public partial class player : CharacterBody2D
 		}
 
 		Velocity = velocity;
+
+		if(Velocity.X < 0) corpo.Scale  = new Vector2(-1, 1);
+		if(Velocity.X > 0) corpo.Scale = new Vector2(1, 1);
+
 		MoveAndSlide();
 	}
 }
