417922b67983c11cfd85e8c0afcf0c769304038e
Tiago de Souza Barros <tiago.barros@EDUC.NET>
2023-04-13 17:33:52 -0300

    rodar arma e iniciando balinhas

--- Scripts/Weapon.cs
+++ Scripts/Weapon.cs
@@ -8,30 +8,52 @@ public partial class Weapon : Node2D
 	public  Node2D ShootPoint;
 	public Sprite2D imagem;
 	[Export]
+	public double intervalShot;
+	[Export]
+	public double shotTimer;
+
+	[Export]
 	public int ammo;
 	[Export]
 	public int maxAmmo;
 	private PackedScene bullet;
+	private Vector2 mousePosition;
 	public override void _Ready()
 	{
-		imagem = GetNode<Sprite2D>("image");
-		ShootPoint = GetNode<Node2D>("ShootPoint");
-		bullet = ResourceLoader.Load<PackedScene>("res://Scenes/regular_bullet.tscn");
+		imagem = GetNode<Sprite2D>("Imagem");
+		ShootPoint =imagem.GetNode<Node2D>("ShootPoint");
+		shotTimer = 0;
+		bullet = ResourceLoader.Load<PackedScene>("res://Scenes/RegularBullet.tscn");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		imagem.LookAt(GetLocalMousePosition());
+		mousePosition = GetGlobalMousePosition();
+
+		imagem.LookAt(mousePosition);
+
+		if(Input.IsActionPressed("Fire")  ) Fire(delta);
+		shotTimer += delta;
+
 	}
 	private void OnBecameInvisible() {
 		QueueFree();		
 	}
-	public void Fire(){
-		 var  tiro  =  bullet.Instantiate();
-		// regular_bullet. = 
-		AddChild(tiro);
-		
-		
+	public void Fire(double delta){
+		if(shotTimer >= (delta + intervalShot)){
+			var  tiro  = (regular_bullet) bullet.Instantiate();
+			var direction = (mousePosition - ShootPoint.Position).Normalized();
+			var angle= Mathf.Atan2(direction.Y, direction.X);
+			tiro.Position = ShootPoint.Position;
+			tiro.Rotation = imagem.Rotation;
+			
+			//tiro.LookAt(GetGlobalMousePosition());
+			
+			
+			AddChild(tiro);
+			shotTimer = 0;
+		}
 	}
 }
+
--- Scripts/regular_bullet.cs
+++ Scripts/regular_bullet.cs
@@ -8,10 +8,14 @@ public partial class regular_bullet : Area2D
 	[Export]
 	public float speed;
 	private Vector2 direction;
+	private Vector2 mousePosition;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		direction = GetLocalMousePosition().Normalized();
+		mousePosition = GetGlobalMousePosition();
+		direction =  ( mousePosition - Position).Normalized();
+		var angle= Mathf.Atan2(direction.Y, direction.X);
+		Rotation = angle;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
